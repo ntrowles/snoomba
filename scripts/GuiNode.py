13d3ce810dd8e4e76ef2da2607a764c97fd81a09
Nicholas Rowles <cpufourproject@gmail.com>
2016-12-12 14:46:33 -0500

    Added requirements.txt

--- scripts/GuiNode.py
+++ scripts/GuiNode.py
@@ -11,9 +11,9 @@ class GuiNode:
         
         
         
-        window.protocol('WM_DELETE_WINDOW', close # Delete node when exiting program
-        window.geometry('{}x{}'.format(1024, 768)) # Resize Window
-        window.mainloop()
+#         window.protocol('WM_DELETE_WINDOW', close # Delete node when exiting program
+#         window.geometry('{}x{}'.format(1024, 768)) # Resize Window
+#         window.mainloop()
 
 gui = GuiNode()
         
