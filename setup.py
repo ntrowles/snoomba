e7d1b25b85557cc5f89f8563dfe108b76e113514
Nicholas Rowles <cpufourproject@gmail.com>
2016-12-12 14:25:47 -0500

    Added setup.py, README, and LICENSE

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='snoomba',
+      version='0.0.1-SNAPSHOT',
+      description='Code for snoomba autonomous snowblower',
+      packages=['snoomba','snoomba.gui'],)
